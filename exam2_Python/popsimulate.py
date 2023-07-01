b6a3cf99f2654fca1515d13bf813e3cfe4b78dbd
HenrryQuiroz <94935202+HenrryQuiroz@users.noreply.github.com>
2023-07-01 10:58:54 +0000

    Henrry Quiroz ha realizado el ejercicio 1.4

--- exam2_Python/popsimulate.py
+++ exam2_Python/popsimulate.py
@@ -45,18 +45,23 @@ def reproduce_population(population): # Se define la función "reproduce_populat
     return new_generation # Devuelve la lista "new_generation" la cual es la nueva generación.
 
 #_________________ Función 4
-def simulate_drift(N, p):
-    my_pop = build_population(N, p)
-    fixation = False
-    num_generations = 0
-    while fixation == False:
-        genotype_counts = compute_frequencies(my_pop)
-        if (genotype_counts["AA"] == N or genotype_counts["aa"] == N):
-            print("An allele reached fixation at generation", num_generations)
-            print("The genotype counts are")
-            print(genotype_counts)
-            fixation == True
-            break
-        my_pop = reproduce_population(my_pop)
-        num_generations += 1
-    return num_generations, genotype_counts
+def simulate_drift(N, p): # Se define la funciòn "simulate_drift" que simulará la deriva genética en una población hasta que uno de los alelos alance la fijación total.
+    # donde se toma como parámetros "N" tamaño de la población y "p" la porbabilidad que un a alelo "a" se sobreponga a un alelo "A".
+    my_pop = build_population(N, p) # Se define la variable "my_pop" donde llama a la función "build_population" donde genera una población de tamaño "N",
+    # y probabilidad "p"
+    fixation = False # Se define la variable "fixation" con el valor "False".
+    num_generations = 0 # Se define una variable "num_generations" con el valor 0 el cual cotará el numero de generaciones que se necesita para alcanzar la fijación.
+    while fixation == False:# Se inicia un bucle "while" el cual seguirá hasta que la variable "fixation" sea "True".
+        genotype_counts = compute_frequencies(my_pop) # Se define la variable "genotype_counts" donde se llama a la función "compute_frecuencies" en la cual calculará las frecuecias,
+        # de los genotipos de "my_pop".
+        if (genotype_counts["AA"] == N or genotype_counts["aa"] == N): # Se genera un bucle donde gracias a la funcíon "genotype_counts" cuenta las frecuencias hasta que uno de los genotipos,
+            # haya llegado a una fijación total, sea homocigoto dominante o recesivo.
+            print("An allele reached fixation at generation", num_generations) # Imprime "An allele reached fixation at generation" y el número de generación a la cual se logró la fijación.
+            print("The genotype counts are") # Imprime el mensaje
+            print(genotype_counts) # Imprime "genotype_counts" el cual muestra las frecuencias genotípicas de esa generación.
+            fixation == True # Establece una operador lógico donde "fixation" es "true"
+            break # rompe el bucle "while"
+        my_pop = reproduce_population(my_pop) # Genera una nueva generación llamando a la función "reproduce_population" con la población actual "my_pop",
+        # donde simula una reproducción y genera una nueva población basada en reproducción aleatoria.
+        num_generations += 1 # Incrementa el contador "num_generations" en 1 para indicar el paso a la siguiente generación.
+    return num_generations, genotype_counts # Devuelve el número de generaciones que se necesitaron para alcanzar la fijación y el conteo de genotipo de la fijación.
